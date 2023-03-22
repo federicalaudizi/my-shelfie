7cf3f20c8f373a8520a5c49e0a962e43f9af8f5b
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2023-03-22 18:45:00 +0100

    New TileUnpickableException added

---
+++ src/main/java/it/polimi/ingsw/server/exceptions/TileUnpickableException.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.server.exceptions;
+
+public class TileUnpickableException extends Exception {
+    public TileUnpickableException() {
+        super();
+    }
+
+    public TileUnpickableException(String message) {
+        super(message);
+    }
+}
