d3c2141a714fed6f2b4218e7e7b6bfa759b89ce3
Federico <fliuzzi02@hotmail.it>
2023-03-22 18:25:27 +0100

    Created Tests for Shelf class

--- src/test/java/it/polimi/ingsw/server/ShelfTest.java
+++ src/test/java/it/polimi/ingsw/server/ShelfTest.java
@@ -4,7 +4,25 @@ import junit.framework.TestCase;
 
 public class ShelfTest extends TestCase {
 
-    public void testIsFull() {
+    public void testIsFull() throws Exception{
+        Shelf testingShelf = new Shelf();
+
+        assertFalse(testingShelf.isFull());
+
+        testingShelf.addTiles(0, new Tile[]{Tile.GATTI, Tile.GATTI, Tile.GATTI});
+        assertFalse(testingShelf.isFull());
+
+        testingShelf.addTiles(0, new Tile[]{Tile.GATTI, Tile.GATTI, Tile.GATTI});
+        testingShelf.addTiles(1, new Tile[]{Tile.GATTI, Tile.GATTI, Tile.GATTI});
+        testingShelf.addTiles(1, new Tile[]{Tile.GATTI, Tile.GATTI, Tile.GATTI});
+        testingShelf.addTiles(2, new Tile[]{Tile.GATTI, Tile.GATTI, Tile.GATTI});
+        testingShelf.addTiles(2, new Tile[]{Tile.GATTI, Tile.GATTI, Tile.GATTI});
+        testingShelf.addTiles(3, new Tile[]{Tile.GATTI, Tile.GATTI, Tile.GATTI});
+        testingShelf.addTiles(3, new Tile[]{Tile.GATTI, Tile.GATTI, Tile.GATTI});
+        testingShelf.addTiles(4, new Tile[]{Tile.GATTI, Tile.GATTI, Tile.GATTI});
+        testingShelf.addTiles(4, new Tile[]{Tile.GATTI, Tile.GATTI, Tile.GATTI});
+        System.out.println(testingShelf);
+        assertTrue(testingShelf.isFull());
     }
 
     public void testGetTile() {
@@ -13,13 +31,29 @@ public class ShelfTest extends TestCase {
     public void testAddTiles() {
     }
 
-    public void testGetTileClusterPoints() {
+    public void testGetTileClusterPoints() throws Exception {
         Shelf testingShelf = new Shelf();
 
-        int zeroPoints = testingShelf.getTileClusterPoints();
-        assertEquals(0, zeroPoints);
+        int testingPoints = testingShelf.getTileClusterPoints();
+        assertEquals(0, testingPoints);
+
+        testingShelf.addTiles(0, new Tile[]{Tile.GATTI, Tile.GATTI, Tile.GATTI});
+        testingPoints = testingShelf.getTileClusterPoints();
+        assertEquals(2, testingPoints);
+
+        testingShelf.addTiles(2, new Tile[]{Tile.GATTI, Tile.GATTI, Tile.GATTI});
+        testingPoints = testingShelf.getTileClusterPoints();
+        assertEquals(4, testingPoints);
     }
 
-    public void testTestToString() {
+    public void testTestToString() throws Exception{
+        Shelf testingShelf = new Shelf();
+
+        System.out.println(testingShelf);
+
+        testingShelf.addTiles(0, new Tile[]{Tile.GATTI, Tile.GATTI, Tile.GATTI});
+        testingShelf.addTiles(0, new Tile[]{Tile.GATTI});
+
+        System.out.println(testingShelf);
     }
 }
