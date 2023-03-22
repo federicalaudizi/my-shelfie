8daad7f3987aa9165f01ca2b6edae5364f395fc9
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2023-03-22 17:13:18 +0100

    Moved Tile class to server package, added default value for out of bounds tiles.

---
+++ src/main/java/it/polimi/ingsw/server/Tile.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.server;
+
+public enum Tile {
+    GATTI("Gatti"),
+    LIBRI("Libri"),
+    GIOCHI("Giochi"),
+    CORNICI("Cornici"),
+    TROFEI("Trofei"),
+    PIANTE("Piante"),
+    OUTSIDE_GAME_BOARD("Outside Game Board");
+
+    private final String type;
+
+    Tile(final String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
