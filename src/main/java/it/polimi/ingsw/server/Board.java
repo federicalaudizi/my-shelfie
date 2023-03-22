6d7189e0ded3f7b08ec1c1474f85db779d2f91c9
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2023-03-22 18:44:07 +0100

    Updated Board class
    - isPickable method implemented (needs exception handling)
    - pickTile method implemented
    - removeTile method implemented
    - checkBoard method implemented
    - repopulate method implemented
    - Javadoc comments now document all methods in the class
    - Added a bag object to Board's attributes

---
+++ src/main/java/it/polimi/ingsw/server/Board.java
@@ -0,0 +1,260 @@
+package it.polimi.ingsw.server;
+
+import it.polimi.ingsw.server.exceptions.TileUnpickableException;
+
+import java.security.InvalidParameterException;
+
+/**
+ * This class creates, initializes and manages the game's board.
+ *
+ * @author Mario Merlo
+ */
+public class Board {
+    private final int MAX_X = 9, MAX_Y = 9;
+    private Tile[][] board;
+    private Bag bag = new Bag();
+
+    /**
+     * Initializes the board for the current game.
+     * @author Mario Merlo
+     *
+     * @param playerNumber The effective dimension of the board depends on the number of players in the game.
+     */
+    Board(int playerNumber){
+        board = new Tile[MAX_X][MAX_Y];
+
+        for(int i = 0; i < MAX_X; i++)
+            for(int j = 0; j < MAX_Y; j++)
+                board[i][j] = null;
+
+        nonPlayableTileInit(playerNumber);
+    }
+
+    /**
+     * Since the game board is tilted by 45°, this method initializes the tiles that are outside the playing
+     * field. This method is only called once per game and assigns to a newly instantiated Board object the special
+     * Tile.OUTSIDE_GAME_BOARD tile to specified coordinates.
+     *
+     * @param playerNumber Different tiles are made available according to the player number for the current game.
+     * @author Mario Merlo
+     */
+    private void nonPlayableTileInit(int playerNumber) {
+        // Upper corners initialization
+        for(int i = 0; i <= 3; i++) {
+            for(int j = 0; j <= 3 - i; j++)
+                board[i][j] = Tile.OUTSIDE_GAME_BOARD;
+            for(int j = 8; j >= 5 + i; j--)
+                board[i][j] = Tile.OUTSIDE_GAME_BOARD;
+        }
+
+        // Lower corners initialization
+        for(int i = 5; i <= 8; i++) {
+            for(int j = 0; j <= i - 5; j++)
+                board[i][j] = Tile.OUTSIDE_GAME_BOARD;
+            for(int j = 8; j >= 8 - i + 5; j--)
+                board[i][j] = Tile.OUTSIDE_GAME_BOARD;
+        }
+
+        switch (playerNumber) {
+            case 2 -> {
+                threePlayerBoardInit(false);
+                fourPlayerBoardInit(false);
+            }
+            case 3 -> {
+                threePlayerBoardInit(true);
+                fourPlayerBoardInit(false);
+            }
+            case 4 -> {
+                threePlayerBoardInit(true);
+                fourPlayerBoardInit(true);
+            }
+        }
+    }
+
+    /**
+     * Renders available or unavailable the tiles that are exclusive to games with three or more players.
+     *
+     * @param atLeastThree This parameter specifies whether there are at least three players in the game. If true, then
+     *                     the tiles are made available; if false, they are initialized as Tile.OUTSIDE_GAME_BOARD.
+     *
+     * @author Mario Merlo
+     */
+    private void threePlayerBoardInit(boolean atLeastThree) {
+        if (!atLeastThree) {
+            board[0][3] = Tile.OUTSIDE_GAME_BOARD;
+            board[2][2] = Tile.OUTSIDE_GAME_BOARD;
+            board[2][6] = Tile.OUTSIDE_GAME_BOARD;
+            board[3][8] = Tile.OUTSIDE_GAME_BOARD;
+            board[5][0] = Tile.OUTSIDE_GAME_BOARD;
+            board[6][2] = Tile.OUTSIDE_GAME_BOARD;
+            board[6][6] = Tile.OUTSIDE_GAME_BOARD;
+            board[8][5] = Tile.OUTSIDE_GAME_BOARD;
+        } else {
+            board[0][3] = null;
+            board[2][2] = null;
+            board[2][6] = null;
+            board[3][8] = null;
+            board[5][0] = null;
+            board[6][2] = null;
+            board[6][6] = null;
+            board[8][5] = null;
+        }
+    }
+
+    /**
+     * Renders available or unavailable the tiles that are exclusive to four player games.
+     *
+     * @param fourPlayers This parameter specifies whether the game has exactly four players. If true, then the four
+     *                    player tiles are made available; if false, they are initialized as Tile.OUTSIDE_GAME_BOARD.
+     *
+     * @author Mario Merlo
+     */
+    private void fourPlayerBoardInit(boolean fourPlayers) {
+        if (!fourPlayers) {
+            board[0][4] = Tile.OUTSIDE_GAME_BOARD;
+            board[1][5] = Tile.OUTSIDE_GAME_BOARD;
+            board[3][1] = Tile.OUTSIDE_GAME_BOARD;
+            board[4][0] = Tile.OUTSIDE_GAME_BOARD;
+            board[4][8] = Tile.OUTSIDE_GAME_BOARD;
+            board[5][7] = Tile.OUTSIDE_GAME_BOARD;
+            board[7][3] = Tile.OUTSIDE_GAME_BOARD;
+            board[8][4] = Tile.OUTSIDE_GAME_BOARD;
+        } else {
+            board[0][4] = null;
+            board[1][5] = null;
+            board[3][1] = null;
+            board[4][0] = null;
+            board[4][8] = null;
+            board[5][7] = null;
+            board[7][3] = null;
+            board[8][4] = null;
+        }
+    }
+
+    /**
+     * Returns whether a tile has a free side and is thus pickable from the board.
+     * @param coord Specifies the coordinate of the tile on the board.
+     * @return true if the tile has at least a side with no other tiles attached, false if not.
+     * @author Mario Merlo
+     */
+
+    //TODO: throw exception when checking outside of matrix boundaries.
+    private boolean isPickable(Coordinate coord) {
+        return  board[coord.getX() + 1][coord.getY()] == null ||
+                board[coord.getX() - 1][coord.getY()] == null ||
+                board[coord.getX()][coord.getY() + 1] == null ||
+                board[coord.getX()][coord.getY() - 1] == null;
+    }
+
+    /**
+     * Returns an array of tiles selected from the board through their coordinates. The method also checks if a tile
+     * is pickable through the method isPickable(). After all tiles are checked, the method removes those tiles from
+     * the board by calling removeTile() on the tile's coordinate.
+     * @param coord1 The coordinate for the first tile.
+     * @param coord2 The coordinate for the second tile.
+     * @param coord3 The coordinate for the third tile.
+     * @return Tile array with the selected tiles.
+     * @throws TileUnpickableException If a tile is deemed unpickable by isPickable(), this exception is thrown.
+     * @throws NullPointerException If coord1 is null, this exception is thrown, because at least one coordinate must
+     * be passed to the method.
+     * @throws InvalidParameterException If removeTile() throws an InvalidParameterException, this is escalated outside
+     * Board.
+     * @author Mario Merlo
+     */
+    Tile[] pickTile(Coordinate coord1, Coordinate coord2, Coordinate coord3) throws TileUnpickableException, NullPointerException, InvalidParameterException {
+        Tile[] pickableTiles = new Tile[3];
+
+        try {
+            if(isPickable(coord1)) {
+                pickableTiles[0] = Tile.valueOf(board[coord1.getX()][coord1.getY()].getType());
+            } else throw new TileUnpickableException();
+        } catch (NullPointerException e) {
+            throw new NullPointerException();
+        }
+
+        if(coord2 != null && isPickable(coord2)) {
+            pickableTiles[1] = Tile.valueOf(board[coord2.getX()][coord2.getY()].getType());
+        } else throw new TileUnpickableException();
+
+        if(coord3 != null && isPickable(coord3)) {
+            pickableTiles[2] = Tile.valueOf(board[coord3.getX()][coord3.getY()].getType());
+        } else throw new TileUnpickableException();
+
+        try {
+            removeTile(coord1);
+            if(coord2 != null) removeTile(coord2);
+            if(coord3 != null) removeTile(coord3);
+        } catch (InvalidParameterException e) {
+            throw new InvalidParameterException();
+        }
+
+        return pickableTiles;
+    }
+
+    /**
+     * Removes a tile from the board by assigning it the null value.
+     * @param coord The coordinate of the tile to be removed.
+     * @throws InvalidParameterException If the coordinates passed identify a non-playing tile, this exception
+     * is thrown.
+     * @author Mario Merlo
+     */
+    private void removeTile(Coordinate coord) throws InvalidParameterException {
+        if(board[coord.getX()][coord.getY()] != Tile.OUTSIDE_GAME_BOARD)
+            board[coord.getX()][coord.getY()] = null;
+        else throw new InvalidParameterException();
+    }
+
+    /**
+     * Checks whether the board needs to be refilled with tiles from the bag. This happens when there are no more
+     * clusters of tiles on the board at least two tiles big.
+     *
+     * @author Mario Merlo
+     */
+    private void checkBoard() {
+        boolean toRepopulate = true;
+
+        // Inner board check: only the first 8 rows and columns are checked, to avoid breaking boundaries.
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 8; j++) {
+                if(board[i][j] != null && board[i][j] != Tile.OUTSIDE_GAME_BOARD)
+                    toRepopulate = (board[i + 1][j] == null && board[i][j + 1] == null) ||
+                                   (board[i + 1][j] == Tile.OUTSIDE_GAME_BOARD && board[i][j + 1] == Tile.OUTSIDE_GAME_BOARD);
+                if(!toRepopulate) break;
+            }
+        }
+
+        // Outliers check: in three and four player modes, these tiles must be checked as well, but it is not necessary
+        // to check them in the same way as for all the other tiles.
+        while(toRepopulate) {
+            if(board[3][8] != Tile.OUTSIDE_GAME_BOARD)
+                toRepopulate = (board[4][8] == null && board[3][7] == null) ||
+                               (board[4][8] == Tile.OUTSIDE_GAME_BOARD && board[3][7] == null);
+            if(board[4][8] != Tile.OUTSIDE_GAME_BOARD)
+                toRepopulate = board[4][7] == null;
+            if(board[8][4] != Tile.OUTSIDE_GAME_BOARD)
+                toRepopulate = board[8][5] == null && board[7][4] == null;
+            if(board[8][5] != Tile.OUTSIDE_GAME_BOARD)
+                toRepopulate = board[7][5] == null;
+        }
+
+        // If toRepopulate remains true up to this point, then there are no more tiles in groups of at least two, thus
+        // the board must be repopulated through the repopulate() method.
+        if(toRepopulate) repopulate();
+    }
+
+    /**
+     * Refills the board with tiles extracted from the bag. Each tile assigned to a new coordinate is a copy of the tile
+     * passed by Bag's extract() method.
+     *
+     * @author Mario Merlo
+     */
+    private void repopulate() {
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                if(board[i][j] == null) {
+                    board[i][j] = Tile.valueOf(bag.extract().getType());
+                }
+            }
+        }
+    }
+}
