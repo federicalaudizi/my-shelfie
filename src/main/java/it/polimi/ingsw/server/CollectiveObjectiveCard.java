d80869236163320eb89d66113b64b7d7df9a6d10
federicalaudizi <federicalaudizi@gmail.com>
2023-03-29 12:42:55 +0200

    Fixed CollectiveObjectiveCard + added org.reflection dependency on POM

--- src/main/java/it/polimi/ingsw/server/CollectiveObjectiveCard.java
+++ src/main/java/it/polimi/ingsw/server/CollectiveObjectiveCard.java
@@ -1,5 +1,12 @@
 package it.polimi.ingsw.server;
+
+import org.reflections.Reflections;
+
+import java.lang.reflect.Modifier;
 import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
 
 /**
  * Abstract class for Collective Objective cards
@@ -9,16 +16,40 @@ import java.util.Random;
 public abstract class CollectiveObjectiveCard {
     public abstract boolean checkObjective(Shelf shelf);
 
-    public int[] pickCard (){
-        Random rand = new Random();
-        int [] ObjectiveCards = {13,13};
-        ObjectiveCards[0] = rand.nextInt(12) + 1;
-        ObjectiveCards[1] = rand.nextInt() + 1;
-        while (ObjectiveCards[0] != ObjectiveCards[1]){
-            ObjectiveCards[1] = rand.nextInt() + 1;
+    public static CollectiveObjectiveCard getRandomCard(CollectiveObjectiveCard other) {
+        List<Class<? extends CollectiveObjectiveCard>> subclasses = getAllPossibleCards();
+
+        Random random = new Random();
+
+        int index = random.nextInt(subclasses.size());
+
+        Class<? extends CollectiveObjectiveCard> subclass = subclasses.get(index);
+
+        while (other.equals(subclass)) {
+            index = random.nextInt(subclasses.size());
+            subclass = subclasses.get(index);
         }
 
-        return ObjectiveCards;
+        try {
+            return subclass.getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static List<Class<? extends CollectiveObjectiveCard>> getAllPossibleCards() {
+        List<Class<? extends CollectiveObjectiveCard>> subclasses = new ArrayList<>();
+
+        Reflections reflections = new Reflections("it.polimi.ingsw.server");
+
+        Set<Class<? extends CollectiveObjectiveCard>> allClasses = reflections.getSubTypesOf(CollectiveObjectiveCard.class);
+
+        for (Class<? extends CollectiveObjectiveCard> clazz : allClasses) {
+            if (!Modifier.isAbstract(clazz.getModifiers())) {
+                subclasses.add(clazz);
+            }
+        }
+        return subclasses;
     }
 }
-
