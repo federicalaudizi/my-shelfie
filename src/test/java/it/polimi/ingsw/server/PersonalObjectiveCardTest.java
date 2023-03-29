1a5a85b616565f07573d0dfb285abd1da237f448
Federico Liuzzi <57435671+fliuzzi02@users.noreply.github.com>
2023-03-29 19:04:45 +0200

    Created PersonalObjectiveCard.checkObjective() test

--- src/test/java/it/polimi/ingsw/server/PersonalObjectiveCardTest.java
+++ src/test/java/it/polimi/ingsw/server/PersonalObjectiveCardTest.java
@@ -1,14 +1,33 @@
 package it.polimi.ingsw.server;
 
+import it.polimi.ingsw.server.exceptions.fullColumnException;
+import it.polimi.ingsw.server.exceptions.notEnoughTilesException;
+import it.polimi.ingsw.server.exceptions.tooManyTilesException;
 import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 
 public class PersonalObjectiveCardTest {
+    @Test
+    public void toStringTest() throws tooManyTilesException, notEnoughTilesException, fullColumnException {
+        PersonalObjectiveCard card = new PersonalObjectiveCard();
+
+        System.out.println(card.toString());
+    }
 
     @Test
-    public void toStringTest(){
+    public void checkObjectectiveTest() throws tooManyTilesException, notEnoughTilesException, fullColumnException {
         PersonalObjectiveCard card = new PersonalObjectiveCard();
-        System.out.println(card.toString());
+        Shelf shelf = new Shelf();
+
+        shelf.addTiles(0, new Tile[]{Tile.GATTI, Tile.GATTI, Tile.GATTI});
+        shelf.addTiles(1, new Tile[]{Tile.GATTI, Tile.GATTI, Tile.GATTI});
+        shelf.addTiles(2, new Tile[]{Tile.GATTI, Tile.GATTI, Tile.GATTI});
+        shelf.addTiles(3, new Tile[]{Tile.GATTI, Tile.GATTI, Tile.GATTI});
+        shelf.addTiles(4, new Tile[]{Tile.GATTI, Tile.GATTI, Tile.GATTI});
+
+        System.out.println(card);
+        System.out.println(shelf);
+        System.out.println(card.checkObjective(shelf));
     }
 }
