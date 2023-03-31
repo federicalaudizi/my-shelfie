3e3dae3cfc0c6530748f784a051bc92e645408bb
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2023-03-31 16:43:12 +0200

    Changed enum names to English; Added colour property.

--- src/main/java/it/polimi/ingsw/server/Tile.java
+++ src/main/java/it/polimi/ingsw/server/Tile.java
@@ -1,18 +1,21 @@
 package it.polimi.ingsw.server;
 
 public enum Tile {
-    GATTI("Gatti"),
-    LIBRI("Libri"),
-    GIOCHI("Giochi"),
-    CORNICI("Cornici"),
-    TROFEI("Trofei"),
-    PIANTE("Piante"),
-    OUTSIDE_GAME_BOARD("Outside Game Board");
+    CATS("Cats", "Green"),
+    BOOKS("Books", "Beige"),
+    GAMES("Games", "Orange"),
+    FRAMES("Frames", "Blue"),
+    TROPHIES("Trophies", "Aqua"),
+    PLANTS("Plants", "Magenta"),
+    OUTSIDE_GAME_BOARD("Outside Game Board", "N/A"),
+    EMPTY("Empty", "N/A");
 
     private final String type;
+    private final String colour;
 
     Tile(final String type) {
         this.type = type;
+        this.colour = colour;
     }
 
     public String getType() {
