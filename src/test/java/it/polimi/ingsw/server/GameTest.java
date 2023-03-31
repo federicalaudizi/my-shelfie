faa5944870113195d48d9150e7a64e1c72e90d07
sassamassa <sara.massarelli@mail.polimi.it>
2023-03-31 18:58:50 +0200

    Modified ChooseFirstPlayer and nextTurn and related tests

--- src/test/java/it/polimi/ingsw/server/GameTest.java
+++ src/test/java/it/polimi/ingsw/server/GameTest.java
@@ -4,7 +4,9 @@ import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
-
+/**Test for Game
+ * @author Sara
+ * */
 public class GameTest {
     Game g2 = new Game(2);
     Game g3 = new Game(3);
@@ -12,23 +14,44 @@ public class GameTest {
         @Test
     public void testNextTurn(){
 
-            //Check if nextTurn() works with two players twice
+            //Check if nextTurn() works with two players
             int previousIndex = g2.getCurrentPlayerIndex();
             g2.nextTurn();
             assertEquals((previousIndex +1)%2, g2.getCurrentPlayerIndex());
             previousIndex = g2.getCurrentPlayerIndex();
             g2.nextTurn();
             assertEquals((previousIndex +1)%2, g2.getCurrentPlayerIndex());
+            previousIndex = g2.getCurrentPlayerIndex();
+            g2.nextTurn();
+            assertEquals((previousIndex +1)%2, g2.getCurrentPlayerIndex());
+            previousIndex = g2.getCurrentPlayerIndex();
+            g2.nextTurn();
+            assertEquals((previousIndex +1)%2, g2.getCurrentPlayerIndex());
 
-            //Check if nextTurn() works with three players twice
+            //Check if nextTurn() works with three players
              previousIndex = g3.getCurrentPlayerIndex();
             g3.nextTurn();
             assertEquals((previousIndex +1)%3, g3.getCurrentPlayerIndex() );
             previousIndex = g3.getCurrentPlayerIndex();
             g3.nextTurn();
             assertEquals((previousIndex +1)%3, g3.getCurrentPlayerIndex());
+            previousIndex = g3.getCurrentPlayerIndex();
+            g3.nextTurn();
+            assertEquals((previousIndex +1)%3, g3.getCurrentPlayerIndex());
+            previousIndex = g3.getCurrentPlayerIndex();
+            g3.nextTurn();
+            assertEquals((previousIndex +1)%3, g3.getCurrentPlayerIndex());
 
-            //Check if nextTurn() works with four players twice
+            //Check if nextTurn() works with four players
+            previousIndex = g4.getCurrentPlayerIndex();
+            g4.nextTurn();
+            assertEquals((previousIndex +1)%4, g4.getCurrentPlayerIndex() );
+            previousIndex = g4.getCurrentPlayerIndex();
+            g4.nextTurn();
+            assertEquals((previousIndex +1)%4, g4.getCurrentPlayerIndex() );
+            previousIndex = g4.getCurrentPlayerIndex();
+            g4.nextTurn();
+            assertEquals((previousIndex +1)%4, g4.getCurrentPlayerIndex() );
             previousIndex = g4.getCurrentPlayerIndex();
             g4.nextTurn();
             assertEquals((previousIndex +1)%4, g4.getCurrentPlayerIndex() );
@@ -41,26 +64,26 @@ public class GameTest {
         public void testChooseFirstPlayer(){
 
             //Checks if chooseFirstPlayer chooses correctly the first player in a game with 2 players and if
-            // assigns correctly the first player seat
-             g2.chooseFirstPlayer(2);
-            System.out.println(g2.getCurrentPlayerIndex());
+            // assigns correctly the first and the last player
+            System.out.println("First Player:" +g2.getFirstPlayerSeat());
             assertTrue(g2.getCurrentPlayerIndex() >=1 && g2.getCurrentPlayerIndex() <=2);
-            assertEquals(g2.getFirstPlayerSeat(), g2.getCurrentPlayerIndex());
+            System.out.println("Last Player:" + g2.getLastPlayer());
+            assertTrue(g2.getLastPlayer() >=1 && g2.getLastPlayer() <=2);
 
 
             //Checks if chooseFirstPlayer chooses correctly the first player in a game with 3 players and if
-            // assigns correctly the first player seat
-            g3.chooseFirstPlayer(3);
-            System.out.println(g3.getCurrentPlayerIndex());
+            // assigns correctly the first and the last player
+            System.out.println("First Player:" + g3.getFirstPlayerSeat());
             assertTrue(g3.getCurrentPlayerIndex() >=1 && g3.getCurrentPlayerIndex() <=3);
-            assertEquals(g3.getFirstPlayerSeat(), g3.getCurrentPlayerIndex());
+            System.out.println("Last Player:" + g3.getLastPlayer());
+            assertTrue(g3.getLastPlayer() >=1 && g3.getLastPlayer() <=3);
 
             //Checks if chooseFirstPlayer chooses correctly the first player in a game with 4 players and if
-            // assigns correctly the first player seat
-              g4.chooseFirstPlayer(4);
-            System.out.println(g4.getCurrentPlayerIndex());
+            // assigns correctly the first and the last player
+            System.out.println("First Player:" +g4.getFirstPlayerSeat());
             assertTrue(g4.getCurrentPlayerIndex() >=1 && g4.getCurrentPlayerIndex() <=4);
-            assertEquals(g4.getFirstPlayerSeat(), g4.getCurrentPlayerIndex());
+            System.out.println("Last Player:" + g4.getLastPlayer());
+            assertTrue(g4.getLastPlayer() >=1 && g4.getLastPlayer() <=4);
         }
 
 
