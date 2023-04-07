0c5f7f289d8763c7d4b40326a18841b78bd9ac93
Federico Liuzzi <57435671+fliuzzi02@users.noreply.github.com>
2023-04-07 22:47:26 +0200

    GameSupervisor now holds GameController instad of Game

--- src/main/java/it/polimi/ingsw/server/controller/GameSupervisor.java
+++ src/main/java/it/polimi/ingsw/server/controller/GameSupervisor.java
@@ -1,6 +1,6 @@
 package it.polimi.ingsw.server.controller;
 
-import it.polimi.ingsw.server.model.Game;
+import it.polimi.ingsw.server.exceptions.NonExsistentGameException;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,7 +15,7 @@ public class GameSupervisor {
     private long lastGameId;
     private final ArrayList<Long> players;
     private final ArrayList<Long> gamesId;
-    private final HashMap<Long, Game> games;
+    private final HashMap<Long, GameController> games; // Relates the game
     private final HashMap<Long, Long> participants;
 
     /**
@@ -45,38 +45,35 @@ public class GameSupervisor {
     }
 
     /**
-     * This method creates a new game and adds it to the list of games.
+     * This method creates a new game and adds it to the list of games. It does not add the creator to the game as a participant
      *
      * @author Federico
      *
-     * @param creatorsID the id of the player who created the game
-     * @param numOfPlayers the number of players in the game
      * @return the id of the new game
      */
-    long newGame(long creatorsID, int numOfPlayers) {
+    long newGame() {
         long newGameId = lastGameId;
         gamesId.add(newGameId);
-        games.put(newGameId, new Game(numOfPlayers));
-        participants.put(creatorsID, newGameId);
+        games.put(newGameId, new GameController());
         lastGameId++;
         return newGameId;
     }
 
     /**
-     * This method adds a player to a game.
+     * This method adds a player to a game
      *
      * @author Federico
      *
      * @param playerId the id of the player
      * @param gameId the id of the game
      * @return true if the game exists, false otherwise
+     * @throws NonExsistentGameException if the game that the player wants to join does not exist
      */
-    boolean joinGame(long playerId, long gameId) {
+    GameController joinGame(long playerId, long gameId) throws NonExsistentGameException {
         if (games.containsKey(gameId)) {
             participants.put(playerId, gameId);
-            return true;
-        }
-        return false;
+            return games.get(gameId);
+        }else throw new NonExsistentGameException();
     }
 
     /**
