34d79ce169b0e41d6928b70532630e78ba7ff5fd
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2023-04-15 09:45:38 +0200

    Small updates to View and ViewCLI

--- src/main/java/it/polimi/ingsw/client/view/View.java
+++ src/main/java/it/polimi/ingsw/client/view/View.java
@@ -1,5 +1,5 @@
 package it.polimi.ingsw.client.view;
 
 public abstract class View {
-
+    public abstract void update();
 }
--- src/main/java/it/polimi/ingsw/client/view/ViewCLI.java
+++ src/main/java/it/polimi/ingsw/client/view/ViewCLI.java
@@ -1,4 +1,20 @@
 package it.polimi.ingsw.client.view;
 
 public class ViewCLI extends View {
+    private String board
+
+    public String getInput() {
+
+    }
+
+    public void printOutput(String output) {
+
+    }
+
+
+
+    @Override
+    public void update() {
+
+    }
 }
