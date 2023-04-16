9227e5de1a241d08b3b96e445458184be53f7373
Federico Liuzzi <57435671+fliuzzi02@users.noreply.github.com>
2023-04-16 20:02:19 +0200

    Added network exceptions

---
+++ src/main/java/it/polimi/ingsw/server/exceptions/AnswerNotReadyException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.server.exceptions;
+
+public class AnswerNotReadyException extends Exception{
+    public AnswerNotReadyException(){super();}
+
+    public AnswerNotReadyException(String message){super(message);}
+}
