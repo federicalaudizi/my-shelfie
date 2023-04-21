eca109bdebccb895bd42e652ea44878ef5136cdf
Federico Liuzzi <57435671+fliuzzi02@users.noreply.github.com>
2023-04-21 09:17:11 +0200

    Added new exception

---
+++ src/main/java/it/polimi/ingsw/server/exceptions/FullGameException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.server.exceptions;
+
+public class FullGameException extends Exception{
+    public FullGameException(){super();}
+
+    public FullGameException(String message){super(message);}
+}
