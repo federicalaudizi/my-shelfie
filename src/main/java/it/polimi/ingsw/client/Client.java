70be364eae184c5d2732f21346fd0add2e14e106
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2023-04-23 17:39:51 +0200

    Added Client abstract class to manage multiple types of clients

---
+++ src/main/java/it/polimi/ingsw/client/Client.java
@@ -0,0 +1,43 @@
+package it.polimi.ingsw.client;
+
+import it.polimi.ingsw.server.controller.network.Message;
+import org.json.*;
+
+/**
+ * This class handles the interactions between the user and the server.
+ *
+ * @author Mario Merlo
+ */
+public abstract class Client {
+    String username;
+    final View view;
+
+    public Client(boolean cli) {
+        if(cli)
+            view = new ViewCLI(this);
+        else
+            view = new ViewGUI(this);
+    }
+
+    abstract void connect();
+    abstract void login();
+    abstract void getMove();
+    abstract JSONObject getReply();
+    abstract void send(Message message);
+
+    /**
+     * Gets the player's username.
+     * @return The player's username.
+     */
+    String getUsername() {
+        return username;
+    }
+
+    /**
+     * Sets the player's username.
+     * @param username The username chosen by the user.
+     */
+    void setUsername(String username) {
+        this.username = username;
+    }
+}
