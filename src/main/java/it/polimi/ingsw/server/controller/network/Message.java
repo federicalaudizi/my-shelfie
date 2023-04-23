654c0f871f23e8d951188efe7d120bdae0d19b66
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2023-04-23 17:43:01 +0200

    Major refactoring to Header names and codes

--- src/main/java/it/polimi/ingsw/server/controller/network/Message.java
+++ src/main/java/it/polimi/ingsw/server/controller/network/Message.java
@@ -50,66 +50,66 @@ public class Message {
      *
      * @author Federico
      */
-    public enum CommandCode {
+    public enum Header {
         /** Welcome Command code */
-        WELCOME("101"),
+        WELCOME(101),
 
         /** Sent by the server when the game ends, the argument should be the leaderboard */
-        GAME_OVER("121"),
+        GAME_OVER(121),
         /** Sent at the end of every turn in order to let the client update the player's view. */
-        GAME_UPDATE("122"),
+        GAME_UPDATE(122),
 
 
         /** An acknowledgement message that is sent when the contents of a previous response are valid */
-        OK("200"),
+        OK(200),
 
         /** Sent by the server when the player requested to join a game, the argument should be a list of active games */
-        GAMES_ID_RESPONSE("211"),
+        GAMES_ID_RESPONSE(211),
         /** Sent by the client when he wants to join an existing game, the argument should be the game id */
-        JOIN_GAME_RESPONSE("211"),
+        JOIN_GAME_RESPONSE(211),
 
         /** Sent by the client when he has to select the tiles, the arguments should be the tiles in order of selections */
-        SEND_TILES("221"),
+        SEND_TILES(221),
         /** Sent by the client has to select the column on the shelf, the argument should be the column */
-        SEND_COLUMN("222"),
+        SEND_COLUMN(222),
 
 
         /** Sent by the client at the first login, argument should be the player id */
-        LOGIN_REQUEST("311"),
+        LOGIN_REQUEST(311),
         /** Sent by the client at reconnection, argument should be the player id */
-        RECONNECT("314"),
+        RECONNECT(314),
         /** Sent by the client when he wants to create a new game, the argument should be the number of players */
-        NEW_GAME_REQUEST("312"),
-        /** Sent by the client if he wants to join an exsisting game */
-        JOIN_GAME_REQUEST("313"),
+        NEW_GAME_REQUEST(312),
+        /** Sent by the client if he wants to join an existing game */
+        JOIN_GAME_REQUEST(313),
 
         /** sent when the client needs to select the tiles */
-        GET_TILES("321"),
+        GET_TILES(321),
         /** sent when the client needs to select the column on the shelf */
-        GET_COLUMN("322"),
+        GET_COLUMN(322),
 
 
         /** Sent by the server when the client sends an invalid command */
-        GENERIC_ERROR("400"),
+        GENERIC_ERROR(400),
 
-        /** Sent by the server to the client if the chosen username already exsits */
-        USERNAME_TAKEN("411"),
+        /** Sent by the server to the client if the chosen username already exists */
+        USERNAME_TAKEN(411),
         /** Sent by the server when the client sends an invalid game id */
-        BAD_GAME_ID("412"),
+        BAD_GAME_ID(412),
         /** Sent by the server when the client selects invalid tiles */
-        BAD_TILES("421"),
+        BAD_TILES(421),
         /** Sent by the server when the client selects an invalid column on the shelf */
-        BAD_COLUMN("422"),
+        BAD_COLUMN(422),
         ;
 
-        private final String code;
+        private final int code;
 
-        CommandCode(String code) {
+        Header(int code) {
             this.code = code;
         }
 
-        public String toString(){
-            return this.code;
+        public int getCode() {
+            return code;
         }
     }
 }
