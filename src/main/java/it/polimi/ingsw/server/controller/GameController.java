ccace33014b9bd5f697cacd783fa33e87be35064
federicalaudizi <federicalaudizi@gmail.com>
2023-04-24 12:06:47 +0200

    connections and disconnections handeld (part I)

--- src/main/java/it/polimi/ingsw/server/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/server/controller/GameController.java
@@ -2,16 +2,11 @@ package it.polimi.ingsw.server.controller;
 
 
 import it.polimi.ingsw.server.controller.network.ClientHandler;
-import it.polimi.ingsw.server.exceptions.TileUnpickableException;
-import it.polimi.ingsw.server.exceptions.fullColumnException;
-import it.polimi.ingsw.server.exceptions.notEnoughTilesException;
-import it.polimi.ingsw.server.exceptions.tooManyTilesException;
+import it.polimi.ingsw.server.exceptions.*;
 import it.polimi.ingsw.server.model.Coordinate;
 import it.polimi.ingsw.server.model.Game;
 import it.polimi.ingsw.server.model.Tile;
 import org.json.JSONObject;
-
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,12 +21,17 @@ import java.util.Map;
 
 public class GameController<T> implements Runnable {
 
+    private final Object lock = new Object();
+
     private final Game game;
     private final Map<String, ClientHandler> playerToClientHandlerMap;
+    private  Map<String, Integer> connectedPlayers;
     private final List<String> turnOrder;
     private final T gameId;
     private boolean isOver;
     private int currentTurnIndex;
+
+
 
       GameController(int playerNumber, T gameId) {
           playerToClientHandlerMap = new HashMap<>();
@@ -43,15 +43,26 @@ public class GameController<T> implements Runnable {
     }
 
 
+    public void notifyDisconnection(String playerId){
+        connectedPlayers.put(playerId, 0);
+    }
+
+    public void notifyConnection(String playerId){
+        connectedPlayers.put(playerId, 1);
+    }
     /**
      * sends the client the game state*/
-    void getGameState() throws IOException {
+    public void getGameState() throws IOException {
           String currentPlayerId = turnOrder.get(currentTurnIndex);
           getClientHandler(currentPlayerId).sendGameState(game.toJson());
     }
 
     /***/
-    void addPlayer(String playerId, ClientHandler handler){
+    void addPlayer(String playerId, ClientHandler handler) throws ReachedMaxNumberOfPlayers {
+        if (playerToClientHandlerMap.size() >= game.getNumberOfPlayers()){
+            throw new ReachedMaxNumberOfPlayers();
+        }
+        connectedPlayers.put(playerId, 1);
         playerToClientHandlerMap.put(playerId, handler);
     }
 
@@ -61,12 +72,6 @@ public class GameController<T> implements Runnable {
     ClientHandler getClientHandler(String playerId){
           return playerToClientHandlerMap.get(playerId);
     }
-
-
-    void setPlayerConnectionStatus(boolean status, T player){
-
-    }
-
 
     /**
      * This method decides who's next turn modifying the currentTurnIndex.
@@ -91,6 +96,29 @@ public class GameController<T> implements Runnable {
             Tile[] tiles;
             Coordinate[] coordinates;
             String currentPlayerId = turnOrder.get(currentTurnIndex);
+
+            if (connectedPlayers.get(currentPlayerId) == 0 && connectedPlayers.values().stream().filter(value -> value == 1).count() > 1){
+                turnHandler();
+                continue;
+            } else if (connectedPlayers.values().stream().filter(value -> value == 1).count() == 1){
+                try {
+                    // Wait for the condition to be verified, with a timeout of 15 seconds
+                    synchronized (lock) {
+                        long startTime = System.currentTimeMillis();
+                        long elapsedTime = 0;
+                        while (connectedPlayers.values().stream().filter(value -> value == 1).count() <= 1 && elapsedTime < 15000) {
+                            lock.wait(5000 - elapsedTime);
+                            elapsedTime = System.currentTimeMillis() - startTime;
+                        }
+                        if (connectedPlayers.values().stream().filter(value -> value == 1).count() <= 1){
+                            getClientHandler(currentPlayerId).gameOver((JSONObject) game.getRankedPlayers());
+                            break;
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
             coordinates = getClientHandler(currentPlayerId).getTiles();
             try {
                 tiles = game.chooseTiles(coordinates[0],coordinates[1],coordinates[2]);
@@ -101,12 +129,20 @@ public class GameController<T> implements Runnable {
             }
             column = getClientHandler(currentPlayerId).getColumn();
             getClientHandler(currentPlayerId).sendOk();
+
             try {
-                game.insertInShelf(column, tiles); //vogliamo avvisare quando qualcuno raggiunge obiettivo comune?
+                int numCollObj = game.insertInShelf(column, tiles);
+                if (numCollObj == 0){
+                    getClientHandler(currentPlayerId).sendGameState(game.toJson());
+                }else{
+                    getClientHandler(currentPlayerId).sendGameState(game.toJson(), numCollObj);
+                }
                 getClientHandler(currentPlayerId).sendOk();
 
             } catch (tooManyTilesException | notEnoughTilesException | fullColumnException e) {
                 getClientHandler(currentPlayerId).badColumn();
+                throw new RuntimeException(e);
+            } catch (IOException e) {
                 throw new RuntimeException(e);
             }
             turnHandler();
