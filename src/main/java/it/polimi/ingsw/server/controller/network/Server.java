e972a4c088f1bf80e77c17b7a402ec1b7a541c85
Federico Liuzzi <57435671+fliuzzi02@users.noreply.github.com>
2023-05-01 17:02:03 +0200

    Server now runs as a thread

--- src/main/java/it/polimi/ingsw/server/controller/network/Server.java
+++ src/main/java/it/polimi/ingsw/server/controller/network/Server.java
@@ -1,5 +1,21 @@
 package it.polimi.ingsw.server.controller.network;
 
-public abstract class Server {
-    public abstract void startServer();
+import it.polimi.ingsw.server.controller.GameSupervisor;
+
+public abstract class Server implements Runnable{
+    GameSupervisor ongoingGames;
+
+    Server(GameSupervisor ongoingGames) {
+        this.ongoingGames = ongoingGames;
+    }
+
+    public static void main(String[] args) {
+        GameSupervisor games = new GameSupervisor();
+
+        Server socketServer = new SocketServer(5000, games);
+        Server rmiServer = new RMIServer(games);
+
+        socketServer.run();
+        rmiServer.run();
+    }
 }
