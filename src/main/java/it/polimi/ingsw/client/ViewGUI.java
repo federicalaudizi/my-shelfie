430aa7f25acf74b4636cebdfb953657b880064db
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2023-05-02 11:51:49 +0200

    Added method stubs

--- src/main/java/it/polimi/ingsw/client/ViewGUI.java
+++ src/main/java/it/polimi/ingsw/client/ViewGUI.java
@@ -1,8 +1,39 @@
 package it.polimi.ingsw.client;
 
+import it.polimi.ingsw.server.model.Player;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class ViewGUI extends View {
     Client client;
     public ViewGUI(Client client) {
         this.client = client;
+    }
+
+    @Override
+    void update(JSONObject gameState) {
+        // TODO Implement
+    }
+
+    @Override
+    String confirmationPrompt(String message) {
+        // TODO Implement
+        return null;
+    }
+
+    @Override
+    void okPrompt(String message) {
+        // TODO Implement
+    }
+
+    @Override
+    void gameOverScreen(ArrayList<Player> leaderboard) {
+        // TODO Implement
+    }
+
+    @Override
+    void gameOverScreen(String winnerUsername) {
+        // TODO Implement
     }
 }
