93241486227a35a0c4a1025ec452c6ef7ca15879
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2023-05-04 11:16:05 +0200

    Optimized imports

--- src/main/java/it/polimi/ingsw/client/ClientSocket.java
+++ src/main/java/it/polimi/ingsw/client/ClientSocket.java
@@ -5,7 +5,10 @@ import it.polimi.ingsw.server.model.Coordinate;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
