4f2c0e5af1f9eeb73b4c8dd790f4668dde1f018e
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2023-05-06 16:00:21 +0200

    Changed getBoard: now calls board.copy

--- src/main/java/it/polimi/ingsw/server/model/Game.java
+++ src/main/java/it/polimi/ingsw/server/model/Game.java
@@ -237,7 +237,7 @@ public class Game {
      * @return the board of the game
      * */
     public Board getBoard(){
-        return new Board(board);
+        return board.copy(board);
     }
 
     /**
