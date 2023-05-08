858923b06026552cca105701eea56bba4b1f9df7
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2023-05-08 08:46:09 +0200

    Added gameTest

--- src/test/java/it/polimi/ingsw/server/controller/ClientSocketTest.java
+++ src/test/java/it/polimi/ingsw/server/controller/ClientSocketTest.java
@@ -1,7 +1,13 @@
 package it.polimi.ingsw.server.controller;
 
+import it.polimi.ingsw.client.Client;
 import it.polimi.ingsw.client.ClientSocket;
+import it.polimi.ingsw.server.controller.network.Server;
+import it.polimi.ingsw.server.controller.network.SocketServer;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -9,7 +15,16 @@ import java.lang.reflect.Method;
 import static org.junit.Assert.*;
 
 public class ClientSocketTest {
-    @Test
+    public static Client client;
+    public static Server server;
+
+    @Before
+    public void init() {
+        client = new ClientSocket(true);
+        server = new SocketServer(5000, new GameSupervisor());
+    }
+
+    @Ignore
     public void validateIpTest() {
         ClientSocket client = new ClientSocket(true);
         try {
@@ -66,5 +81,9 @@ public class ClientSocketTest {
         }
     }
 
-
+    @Test
+    public void gameTest() {
+        client.start();
+        server.run();
+    }
 }
