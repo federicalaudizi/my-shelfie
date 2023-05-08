13761e9b4442c9a9fbbf4212f50f716336278d99
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2023-05-08 09:32:39 +0200

    Started testing ViewCLI

--- src/test/java/it/polimi/ingsw/server/controller/ViewCLITest.java
+++ src/test/java/it/polimi/ingsw/server/controller/ViewCLITest.java
@@ -1,42 +1,75 @@
 package it.polimi.ingsw.server.controller;
 
+import it.polimi.ingsw.client.Client;
+import it.polimi.ingsw.client.ClientSocket;
+import it.polimi.ingsw.client.View;
+import it.polimi.ingsw.client.ViewCLI;
+import it.polimi.ingsw.server.model.Game;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.regex.Pattern;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import static org.junit.Assert.fail;
 
 public class ViewCLITest {
+    View view;
+    Game twoPlayerGame, threePlayerGame, fourPlayerGame;
+    LinkedList<String> playerOrder = new LinkedList<>();
+    ArrayList<String> usernames = new ArrayList<>();
+
+    @Before
+    public void init() {
+        // Initialize games
+        twoPlayerGame = new Game(2);
+        threePlayerGame = new Game(3);
+        fourPlayerGame = new Game(4);
+
+        // Add the first two players
+        playerOrder.add("Mario");
+        usernames.add("Mario");
+        playerOrder.add("Martina");
+        usernames.add("Martina");
+
+        // Set the player usernames in twoPlayerGame
+        twoPlayerGame.setUsernames(usernames);
+
+        // Initialize a client with the username of the first player and get its view
+        Client client = new ClientSocket(true, "Mario");
+        view = client.getView();
+    }
+
     @Test
-    public void viewSubstitutionTest() {
-        String twoPlayerView = """
-            You (?)         | Board             | Your objective:
-            * * * * *       | ^ ^ ^ ^ ^ ^ ^ ^ ^ | $ $ $ $ $
-            * * * * *       | ^ ^ ^ ^ ^ ^ ^ ^ ^ | $ $ $ $ $
-            * * * * *       | ^ ^ ^ ^ ^ ^ ^ ^ ^ | $ $ $ $ $
-            * * * * *       | ^ ^ ^ ^ ^ ^ ^ ^ ^ | $ $ $ $ $
-            * * * * *       | ^ ^ ^ ^ ^ ^ ^ ^ ^ | $ $ $ $ $
-            * * * * *       | ^ ^ ^ ^ ^ ^ ^ ^ ^ | $ $ $ $ $
-            --------------- | ^ ^ ^ ^ ^ ^ ^ ^ ^ | ---------------
-            ¥2       (?)    | ^ ^ ^ ^ ^ ^ ^ ^ ^ |
-            @ @ @ @ @       | ^ ^ ^ ^ ^ ^ ^ ^ ^ |
-            @ @ @ @ @       | ----------------- |
-            @ @ @ @ @       |                   |
-            @ @ @ @ @       |                   |
-            @ @ @ @ @       |                   |
-            @ @ @ @ @       |                   |
-            -----------------------------------------------------
-            Objective I (&1): £1
-            Objective II (&2): £2
-        """;
-        System.out.println(twoPlayerView);
-        String userShelf = "eeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
-        String playerTwoShelf = "222222222222222222222222222222";
-        String view = twoPlayerView;
-        for(int i = 0; i < 81; i++)
-            view = Pattern.compile("\\^").matcher(view).replaceFirst("b");
-        for(int i = 0; i < 30; i++) {
-            view = Pattern.compile("\\*").matcher(view).replaceFirst(userShelf.substring(i, i + 1));
-            view = Pattern.compile("@").matcher(view).replaceFirst(playerTwoShelf.substring(i, i + 1));
+    public void composeViewTest() {
+        try {
+            // Initialize private composeView method
+            Method composeViewMethod = ViewCLI.class.getDeclaredMethod("composeView", Game.class, LinkedList.class);
+            composeViewMethod.setAccessible(true);
+
+            // Call composeView on twoPlayerGame
+            composeViewMethod.invoke(view, twoPlayerGame, playerOrder);
+
+            // Add the third player and set usernames in threePlayerGame
+            playerOrder.add("Margherita");
+            usernames.add("Margherita");
+            threePlayerGame.setUsernames(usernames);
+
+            // Call composeView on threePlayerGame
+            composeViewMethod.invoke(view, threePlayerGame, playerOrder);
+
+            // Add the fourth player and set usernames in fourPlayerGame
+            playerOrder.add("Camilla");
+            usernames.add("Camilla");
+            fourPlayerGame.setUsernames(usernames);
+
+            // Call composeView on fourPlayerGame
+            composeViewMethod.invoke(view, fourPlayerGame, playerOrder);
+
+        } catch (Exception e) {
+            fail();
         }
-        System.out.println(view);
+
     }
 }
