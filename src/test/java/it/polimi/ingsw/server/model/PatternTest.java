729ecde574de2781d8f3582f8bb606ef2b05c30f
federicalaudizi <federicalaudizi@gmail.com>
2023-05-08 15:49:54 +0200

    Added CollectiveObjectiveCard tests

--- src/test/java/it/polimi/ingsw/server/model/PatternTest.java
+++ src/test/java/it/polimi/ingsw/server/model/PatternTest.java
@@ -255,4 +255,46 @@ public class PatternTest {
         System.out.println("Random Card: " + card);
     }
 
+    @Test
+    public void testGetRandomCard1() {
+        CollectiveObjectiveCard card = CollectiveObjectiveCard.getRandomCard();
+        System.out.println("Random Card: " + card);
+
+        assert card != null;
+        CollectiveObjectiveCard cc = CollectiveObjectiveCard.getRandomCard(card);
+        System.out.println("second random card: "+ cc);
+
+        CollectiveObjectiveCard card1 = CollectiveObjectiveCard.getRandomCard();
+        System.out.println("Random Card: " + card1);
+
+        assert card1 != null;
+        CollectiveObjectiveCard cc1 = CollectiveObjectiveCard.getRandomCard(card1);
+        System.out.println("second random card: "+ cc1);
+
+        CollectiveObjectiveCard card2 = CollectiveObjectiveCard.getRandomCard();
+        System.out.println("Random Card: " + card2);
+
+        assert card2 != null;
+        CollectiveObjectiveCard cc2 = CollectiveObjectiveCard.getRandomCard(card2);
+        System.out.println("second random card: "+ cc2);
+
+        CollectiveObjectiveCard card3 = CollectiveObjectiveCard.getRandomCard();
+        System.out.println("Random Card: " + card3);
+
+        CollectiveObjectiveCard cc3 = CollectiveObjectiveCard.getRandomCard(card3);
+        System.out.println("second random card: "+ cc3);
+
+        CollectiveObjectiveCard card4 = CollectiveObjectiveCard.getRandomCard();
+        System.out.println("Random Card: " + card4);
+
+        CollectiveObjectiveCard cc4 = CollectiveObjectiveCard.getRandomCard(card4);
+        System.out.println("second random card: "+ cc4);
+
+        CollectiveObjectiveCard card5 = CollectiveObjectiveCard.getRandomCard();
+        System.out.println("Random Card: " + card5);
+
+        CollectiveObjectiveCard cc5 = CollectiveObjectiveCard.getRandomCard(card5);
+        System.out.println("second random card: "+ cc5);
+    }
+
 }
