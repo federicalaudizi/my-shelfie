4c880d98e839df9e1b605d667683722db21fdbe1
federicalaudizi <federicalaudizi@gmail.com>
2023-05-13 17:04:28 +0200

    Add connection to welcome button

--- src/main/java/it/polimi/ingsw/client/Gui.java
+++ src/main/java/it/polimi/ingsw/client/Gui.java
@@ -1,14 +1,20 @@
 package it.polimi.ingsw.client;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Gui extends Application {
 
-    private static Stage stage;
+    @FXML
+    private Button mainPane;
+    static Stage stage;
 
     /* Code for JavaFX application. (Stage, scene, scene graph) */
     @Override
@@ -24,6 +30,24 @@ public class Gui extends Application {
         stage.setResizable(false);
         stage.show();
 
+    }
+
+    @FXML
+    private void handleStartGameButtonClick() {
+        try {
+            // Load the second view FXML file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Username.fxml"));
+            Parent secondViewParent =loader.load();
+            Scene secondViewScene = new Scene(secondViewParent);
+
+            // Get the Stage object from the current view
+            javafx.stage.Stage currentStage = (javafx.stage.Stage) mainPane.getScene().getWindow();
+
+            // Set the new scene on the stage
+            currentStage.setScene(secondViewScene);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     //Launches the GUI
