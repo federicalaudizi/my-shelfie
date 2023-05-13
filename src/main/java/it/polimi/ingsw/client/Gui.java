c2d7ba9823eaaf9c5122c66117e0f9d105341820
federicalaudizi <federicalaudizi@gmail.com>
2023-05-13 17:26:23 +0200

    Fix warnings on Gui

--- src/main/java/it/polimi/ingsw/client/Gui.java
+++ src/main/java/it/polimi/ingsw/client/Gui.java
@@ -8,12 +8,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
 public class Gui extends Application {
 
-    @FXML
-    private Button mainPane;
+
     static Stage stage;
 
     /* Code for JavaFX application. (Stage, scene, scene graph) */
@@ -24,11 +22,6 @@ public class Gui extends Application {
         // Load the FXML file with the FXMLLoader
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Welcome.fxml"));
         Parent root = fxmlLoader.load();
-
-        // Retrieve the WelcomeController instance from the FXMLLoader
-        WelcomeController welcomeController = fxmlLoader.getController();
-
-        // Perform any necessary initialization or setup on the WelcomeController
 
         // Construct the Scene using the loaded Parent and set it on the Stage
         Scene scene = new Scene(root);
