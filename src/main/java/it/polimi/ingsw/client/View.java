05cc65b78e74c61c3a2a3b9d7349f27597082419
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2023-05-15 16:52:42 +0200

    Optimize imports

--- src/main/java/it/polimi/ingsw/client/View.java
+++ src/main/java/it/polimi/ingsw/client/View.java
@@ -1,26 +1,10 @@
 package it.polimi.ingsw.client;
 
-import it.polimi.ingsw.server.model.Board;
 import it.polimi.ingsw.server.model.Game;
 import org.json.JSONArray;
 
-import it.polimi.ingsw.server.model.Player;
-import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.effect.GaussianBlur;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
-
-import java.io.IOException;
-import java.util.*;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.concurrent.SynchronousQueue;
 
 
