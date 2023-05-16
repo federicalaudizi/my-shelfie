ea5b2a25942150373690ff3e60c27b2c30145c2d
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2023-05-16 16:26:49 +0200

    Add RMIGameInterface method signatures

--- src/main/java/it/polimi/ingsw/server/controller/network/rmi/RMIGameInterface.java
+++ src/main/java/it/polimi/ingsw/server/controller/network/rmi/RMIGameInterface.java
@@ -1,6 +1,12 @@
 package it.polimi.ingsw.server.controller.network.rmi;
 
+import it.polimi.ingsw.server.controller.network.Message;
+
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface RMIGameInterface extends Remote {
+    Message ping(String username) throws RemoteException;
+    Message submitTiles(String username, Message tileMessage) throws RemoteException;
+    Message submitColumn(String username, Message columnMessage) throws RemoteException;
 }
