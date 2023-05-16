dd356acddce8e7e6e62ae4ee22ceec2aacf7173a
Federico Liuzzi <57435671+fliuzzi02@users.noreply.github.com>
2023-05-16 12:07:06 +0200

    change visibility of methods

--- src/main/java/it/polimi/ingsw/server/controller/network/Server.java
+++ src/main/java/it/polimi/ingsw/server/controller/network/Server.java
@@ -3,9 +3,9 @@ package it.polimi.ingsw.server.controller.network;
 import it.polimi.ingsw.server.controller.GameSupervisor;
 
 public abstract class Server implements Runnable{
-    GameSupervisor ongoingGames;
+    protected GameSupervisor ongoingGames;
 
-    Server(GameSupervisor ongoingGames) {
+    protected Server(GameSupervisor ongoingGames) {
         this.ongoingGames = ongoingGames;
     }
 }
