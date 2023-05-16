483a61ea4078891265f61b02a71ab0622f289b48
Federico Liuzzi <57435671+fliuzzi02@users.noreply.github.com>
2023-05-16 12:35:57 +0200

    add javaDoc

--- src/main/java/it/polimi/ingsw/server/controller/network/rmi/RMILoginInterface.java
+++ src/main/java/it/polimi/ingsw/server/controller/network/rmi/RMILoginInterface.java
@@ -1,14 +1,60 @@
 package it.polimi.ingsw.server.controller.network.rmi;
 
+import it.polimi.ingsw.server.controller.GameSupervisor;
 import it.polimi.ingsw.server.controller.network.Message;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface RMILoginInterface extends Remote {
+    /**
+     * This method is used to log in a player, it expects a username and returns a message with an error or confirmation
+     *
+     * @param username the username of the player
+     * @return a message with an error or confirmation, confirmation consist in OK, errors can be USERNAME_TAKEN or GENERIC_ERROR
+     * @throws RemoteException
+     * @author Federico
+     */
     Message login(String username) throws RemoteException;
+
+    /**
+     * This method is used to reconnect a player, it expects a username and returns a message with an error or confirmation
+     *
+     * @param username the username of the player
+     * @return a message with an error or confirmation, confirmation consist in OK, error could be GENERIC_ERROR
+     * @throws RemoteException
+     * @author Federico
+     */
     Message reconnect(String username) throws RemoteException;
+
+    /**
+     * This method is used to create a game, it expects a username and the number of players, it returns a message with an error or confirmation
+     *
+     * @param me the username of the player
+     * @param numberOfPlayers the number of players
+     * @return a message with an error or confirmation, confirmation consist in OK, error can be GENERIC_ERROR
+     * @throws RemoteException
+     * @author Federico
+     */
     Message createGame(String me, int numberOfPlayers) throws RemoteException;
+
+    /**
+     * This method is used to get the list of games, it expects a username and returns a message with an error or confirmation
+     *
+     * @param me the username of the player
+     * @return a message with the list of all games
+     * @throws RemoteException
+     * @author Federico
+     */
     Message getGameList(String me) throws RemoteException;
-    Message joinGame(String gameID) throws RemoteException;
+
+    /**
+     * This method is used to join a game, it expects a username and a gameId and returns a message with an error or confirmation
+     *
+     * @param me the username of the player
+     * @param gameID the id of the game
+     * @return a message with an error or confirmation, confirmation consist in OK, error can be GENERIC_ERROR
+     * @throws RemoteException
+     */
+    Message joinGame(String me, String gameID) throws RemoteException;
 }
