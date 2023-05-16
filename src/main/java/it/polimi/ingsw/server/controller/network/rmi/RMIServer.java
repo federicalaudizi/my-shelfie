c9c4fc1d96c716ca191667ee6bbc3493e86b6ba3
Federico Liuzzi <57435671+fliuzzi02@users.noreply.github.com>
2023-05-16 12:44:55 +0200

    modify RMI server

--- src/main/java/it/polimi/ingsw/server/controller/network/rmi/RMIServer.java
+++ src/main/java/it/polimi/ingsw/server/controller/network/rmi/RMIServer.java
@@ -2,9 +2,7 @@ package it.polimi.ingsw.server.controller.network.rmi;
 
 import it.polimi.ingsw.server.controller.GameSupervisor;
 import it.polimi.ingsw.server.controller.network.Server;
-import it.polimi.ingsw.server.exceptions.ReachedMaxNumberOfPlayers;
 
-import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
@@ -20,20 +18,20 @@ public class RMIServer extends Server {
     public void run() {
         try {
 
-            RMILogin login = new RMILogin();
+            RMILogin login = new RMILogin(ongoingGames);
             RMIGame game = new RMIGame();
 
             RMILoginInterface loginSkeleton = (RMILoginInterface) UnicastRemoteObject.exportObject(login, 0);
             RMIGameInterface gameSkeleton = (RMIGameInterface) UnicastRemoteObject.exportObject(game, 0);
 
-            Registry registry = LocateRegistry.createRegistry(5000);
+            Registry registry = LocateRegistry.getRegistry();
 
             registry.bind("RMILoginInterface", loginSkeleton);
             registry.bind("RMIGameInterface", gameSkeleton);
 
             System.out.println("Server ready.");
         } catch (Exception e) {
-            System.err.println("Server exception: " + e.toString());
+            System.err.println("Server exception: " + e);
             e.printStackTrace();
         }
     }
