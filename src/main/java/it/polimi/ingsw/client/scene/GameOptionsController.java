adc960b31e1744cff0508d764daa2dfb9b5590dd
federicalaudizi <federicalaudizi@gmail.com>
2023-05-17 23:59:46 +0200

    Adjust GameOptionsController for button

--- src/main/java/it/polimi/ingsw/client/scene/GameOptionsController.java
+++ src/main/java/it/polimi/ingsw/client/scene/GameOptionsController.java
@@ -2,42 +2,51 @@ package it.polimi.ingsw.client.scene;
 
 import it.polimi.ingsw.client.ViewGUI;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 
 public class GameOptionsController {
     @FXML
-    public ChoiceBox choiceSelector;
+    public ChoiceBox<String> choiceSelector;
+    @FXML
+    public Button button;
 
-    public void chooseOption(){
-        choiceSelector.setOnAction(event -> {
-            String selectedOption = (String) choiceSelector.getValue();
 
-            if (selectedOption.equals("Create a new game")) {
-                try {
-                    //give nick to manager thread
-                    ViewGUI.queue.put(1);
+    public void chooseOption() {
+        String selectedOption = choiceSelector.getValue();
+        button.setVisible(true);
 
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
+        button.setOnAction(event -> {
+
+            switch (selectedOption) {
+                case "Create a new game" -> {
+                    try {
+                        //give nick to manager thread
+                        ViewGUI.queue.put(1);
+
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
                 }
-            } else if (selectedOption.equals("Join a new game")) {
-                try {
-                    //give nick to manager thread
-                    ViewGUI.queue.put(2);
+                case "Join a new game" -> {
+                    try {
+                        //give nick to manager thread
+                        ViewGUI.queue.put(2);
 
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
                 }
-            } else if (selectedOption.equals("Reconnect to an ongoing game")) {
-                try {
-                    //give nick to manager thread
-                    ViewGUI.queue.put(3);
+                case "Reconnect to an ongoing game" -> {
+                    try {
+                        //give nick to manager thread
+                        ViewGUI.queue.put(3);
 
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
                 }
             }
         });
-
     }
 }
