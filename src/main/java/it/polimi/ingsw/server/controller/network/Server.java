2402c670dee87d25db304f79b9acbb45d75f6cc3
Federico Liuzzi <57435671+fliuzzi02@users.noreply.github.com>
2023-05-20 17:41:21 +0200

    add port number variable to superclass

--- src/main/java/it/polimi/ingsw/server/controller/network/Server.java
+++ src/main/java/it/polimi/ingsw/server/controller/network/Server.java
@@ -4,8 +4,10 @@ import it.polimi.ingsw.server.controller.GameSupervisor;
 
 public abstract class Server implements Runnable{
     protected GameSupervisor ongoingGames;
+    protected int port;
 
-    protected Server(GameSupervisor ongoingGames) {
+    protected Server(int port, GameSupervisor ongoingGames) {
+        this.port = port;
         this.ongoingGames = ongoingGames;
     }
 }
