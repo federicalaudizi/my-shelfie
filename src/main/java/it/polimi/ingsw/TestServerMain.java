88a2635afea45c85f24ccfdaf2d4417594c027d2
Federico Liuzzi <57435671+fliuzzi02@users.noreply.github.com>
2023-05-20 17:46:35 +0200

    add threading and rmi server

--- src/main/java/it/polimi/ingsw/TestServerMain.java
+++ src/main/java/it/polimi/ingsw/TestServerMain.java
@@ -2,14 +2,17 @@ package it.polimi.ingsw;
 
 import it.polimi.ingsw.server.controller.GameSupervisor;
 import it.polimi.ingsw.server.controller.network.Server;
+import it.polimi.ingsw.server.controller.network.rmi.RMIServer;
 import it.polimi.ingsw.server.controller.network.socket.SocketServer;
 
 public class TestServerMain {
     public static void main(String[] args) {
         GameSupervisor games = new GameSupervisor();
 
-        Server socketServer = new SocketServer(8000, games);
+        Server socketServer = new SocketServer(5000, games);
+        Server rmiServer = new RMIServer(1099, games);
 
-        socketServer.run();
+        new Thread(socketServer).start();
+        new Thread(rmiServer).start();
     }
 }
