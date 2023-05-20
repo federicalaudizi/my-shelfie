3084aedd334c343a9f50712f845935416d759fef
Federico Liuzzi <57435671+fliuzzi02@users.noreply.github.com>
2023-05-20 17:44:08 +0200

    add javadoc

--- src/main/java/it/polimi/ingsw/server/controller/network/Server.java
+++ src/main/java/it/polimi/ingsw/server/controller/network/Server.java
@@ -6,6 +6,12 @@ public abstract class Server implements Runnable{
     protected GameSupervisor ongoingGames;
     protected int port;
 
+    /**
+     * Creates a new instance of the server on the specified port
+     * @param port the port on which the server will be listening
+     * @param ongoingGames the GameSupervisor instance
+     * @author Federico
+     */
     protected Server(int port, GameSupervisor ongoingGames) {
         this.port = port;
         this.ongoingGames = ongoingGames;
