43dd6b7dc03c6c9af2ce1c9aefbd4c562f0d34a1
Federico Liuzzi <57435671+fliuzzi02@users.noreply.github.com>
2023-05-21 15:15:58 +0200

    add concrete method

--- src/test/java/it/polimi/ingsw/server/controller/FakeClientHandler.java
+++ src/test/java/it/polimi/ingsw/server/controller/FakeClientHandler.java
@@ -33,6 +33,18 @@ public class FakeClientHandler extends ClientHandler {
     }
 
     /**
+     * This method sends the first gamestate to the client
+     *
+     * @param gameState       the gamestate to send to the client
+     * @param player          the player that completed the objective
+     * @param gainedObjective 1 if the player gained the first objective, 2 if the player gained the second objective, 3 if the player gained both objectives
+     */
+    @Override
+    public void sendGameState(Game gameState, String player, int gainedObjective) {
+
+    }
+
+    /**
      * This method signals the client that a response was accepted
      *
      * @author Sara
