58a433193179e664e6070f7c7c9b5094b905944e
sassamassa <sara.massarelli13@gmail.com>
2023-05-21 16:33:13 +0200

    Fix gameIdSelection

--- src/main/java/it/polimi/ingsw/client/ViewGUI.java
+++ src/main/java/it/polimi/ingsw/client/ViewGUI.java
@@ -77,7 +77,7 @@ public class ViewGUI extends View {
     String gameIdSelection(ArrayList<String> gameIds) {
         String selectedGame = null;
         Platform.runLater(() -> Gui.getStage().setScene(new Scene(joinRoot)));
-        JoinController.addGameIds(gameIds);
+        joinController.addGameIds(gameIds);
         try {
             selectedGame = (String) queue.take();
         } catch (InterruptedException e) {
