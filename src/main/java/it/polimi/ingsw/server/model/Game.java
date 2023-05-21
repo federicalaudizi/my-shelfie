eb90c19d13667a045f05106e08e7c9d287c01ee0
Federico Liuzzi <57435671+fliuzzi02@users.noreply.github.com>
2023-05-21 15:05:20 +0200

    change Game.insertInShelf signature

--- src/main/java/it/polimi/ingsw/server/model/Game.java
+++ src/main/java/it/polimi/ingsw/server/model/Game.java
@@ -178,15 +178,13 @@ public class Game {
      *
      * @param column of the shelf where to place the tiles
      * @param tiles  to place in the shelf
-     * @return 0 if the player has not achieved any goal, 1 if the player has achieved the first goal, 2 if the player has achieved the second goal, 3 if the player has achieved both goals
      */
-    public int insertInShelf(int column, Tile[] tiles) throws tooManyTilesException, notEnoughTilesException, fullColumnException {
+    public void insertInShelf(int column, Tile[] tiles) throws tooManyTilesException, notEnoughTilesException, fullColumnException {
         players.get(currentPlayerIndex).addPlayerTiles(column, tiles);
         if (players.get(currentPlayerIndex).getShelf().isFull()) {
             lastTurn = true;
             players.get(currentPlayerIndex).setEndGameCard();
         }
-        return checkGoals();
     }
 
     /**
