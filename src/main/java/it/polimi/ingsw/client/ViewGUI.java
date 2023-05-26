1fb09d29745d347803c1cf185e9e23573eb52087
sassamassa <sara.massarelli13@gmail.com>
2023-05-26 15:14:03 +0200

    Add initializeShelves

--- src/main/java/it/polimi/ingsw/client/ViewGUI.java
+++ src/main/java/it/polimi/ingsw/client/ViewGUI.java
@@ -77,7 +77,7 @@ public class ViewGUI extends View {
         setPersonalObjectiveCard(game);
         gameController.setShelves(game);
         gameController.initializeBoard(game);
-        //gameController.initializeShelves(game);
+        gameController.initializeShelves(game, this.client);
         Platform.runLater(() -> {
             Gui.getStage().setTitle(client.getUsername());
             Scene currentScene = Gui.getStage().getScene();
