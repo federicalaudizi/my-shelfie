c79898f18e87bc6f911fa6c0ed3dcdcb07a6859c
Federico Liuzzi <57435671+fliuzzi02@users.noreply.github.com>
2023-05-30 18:45:27 +0200

    add gamesaves dir creation functionality

--- src/main/java/it/polimi/ingsw/server/controller/GameSupervisor.java
+++ src/main/java/it/polimi/ingsw/server/controller/GameSupervisor.java
@@ -9,6 +9,9 @@ import org.json.JSONObject;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -56,6 +59,12 @@ public class GameSupervisor implements Runnable{
 
     @Override
     public void run() {
+        Path path = Paths.get("gamesaves/");
+        try {
+            Files.createDirectory(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         while(true){
             try {
                 synchronized (this) {
