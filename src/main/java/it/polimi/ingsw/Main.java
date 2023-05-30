2cf6844f01908458d8706c5bb80e7039fa5c9c91
Federico Liuzzi <57435671+fliuzzi02@users.noreply.github.com>
2023-05-30 14:27:37 +0200

    modified main to be posix compliant

--- src/main/java/it/polimi/ingsw/Main.java
+++ src/main/java/it/polimi/ingsw/Main.java
@@ -18,22 +18,27 @@ class Main {
     public static void main (String[] args) throws Exception {
         System.out.println("Welcome to MyShelfie!!");
 
-        if (args[0].equals("client")){
+        if (args.length == 0){
+            System.out.println("No arguments provided");
+            return;
+        }
+
+        if (args[0].equals("--client")){
             Client c;
-            if(args[1].equals("socket")){
-                if (args[2].equals("gui")){
+            if(args[1].equals("--socket")){
+                if (args[2].equals("--gui")){
                     //Image image = new Image("file:src/main/resources/Images/eight.jpg");
                     c = new ClientSocket(false);
-                }else if (args[2].equals("tui")){
+                }else if (args[2].equals("--tui")){
                     c = new ClientSocket(true);
                 }else {
                     System.out.println("invalid option -- " + args[2] + "\n" + "To be provided: 'gui' or 'tui'");
                     return;
                 }
-            } else if (args[1].equals("rmi")){
-                if (args[2].equals("gui")){
+            } else if (args[1].equals("--rmi")){
+                if (args[2].equals("--gui")){
                     c = new ClientRMI(false);
-                }else if (args[2].equals("tui")){
+                }else if (args[2].equals("--tui")){
                     c = new ClientRMI(true);
                 } else {
                     System.out.println("invalid option -- " + args[2] + "\n" + "To be provided: 'gui' or 'tui'");
@@ -44,13 +49,13 @@ class Main {
                 return;
             }
             c.start();
-        } else if (args[0].equals("server")){
+        } else if (args[0].equals("--server")){
             GameSupervisor gameSupervisor = new GameSupervisor();
             new Thread(gameSupervisor).start();
             switch (args[1]) {
-                case "socket" -> new Thread(new SocketServer(8000, gameSupervisor)).start();
-                case "rmi" -> new Thread(new RMIServer(1099, gameSupervisor)).start();
-                case "both" -> {
+                case "--socket" -> new Thread(new SocketServer(8000, gameSupervisor)).start();
+                case "--rmi" -> new Thread(new RMIServer(1099, gameSupervisor)).start();
+                case "--both" -> {
                     new Thread(new SocketServer(8000, gameSupervisor)).start();
                     new Thread(new RMIServer(1099, gameSupervisor)).start();
                 }
