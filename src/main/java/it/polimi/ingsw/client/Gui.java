f59745d613c8ff22d8fb179a5d31b1f1b3322e63
sassamassa <sara.massarelli13@gmail.com>
2023-05-30 18:34:26 +0200

    Add closing process

--- src/main/java/it/polimi/ingsw/client/Gui.java
+++ src/main/java/it/polimi/ingsw/client/Gui.java
@@ -1,10 +1,12 @@
 package it.polimi.ingsw.client;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 
 public class Gui extends Application {
@@ -25,6 +27,12 @@ public class Gui extends Application {
         Scene scene = new Scene(root);
         stage.setResizable(false);
         stage.setScene(scene);
+
+        stage.setOnCloseRequest((WindowEvent t) -> {
+            Platform.exit();
+            System.exit(0);
+        });
+
         stage.show();
     }
 
