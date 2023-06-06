29230018c5a369df81f99260a4847394180e6a04
sassamassa <sara.massarelli13@gmail.com>
2023-06-06 22:45:33 +0200

    modify GameOptions.fxml with buttons

--- src/main/java/it/polimi/ingsw/client/scene/GameOptionsController.java
+++ src/main/java/it/polimi/ingsw/client/scene/GameOptionsController.java
@@ -3,50 +3,47 @@ package it.polimi.ingsw.client.scene;
 import it.polimi.ingsw.client.ViewGUI;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 
 public class GameOptionsController {
-    @FXML
-    public ChoiceBox<String> choiceSelector;
+
     @FXML
     public Button button;
 
+
     /**
-     * The player can decide if create a new game, join an existing game or to reconnect to an old one
-     * This method lets the player chose and sends the choice to the manager thread as a number
-     * */
-    public void chooseOption() {
-        String selectedOption = choiceSelector.getValue();
-        button.setVisible(true);
-
-        button.setOnAction(event -> {
-
-            switch (selectedOption) {
-                case "Create a new game" -> {
-                    try {
-                        ViewGUI.queue.put(1);
-
-                    } catch (InterruptedException ex) {
-                        ex.printStackTrace();
-                    }
-                }
-                case "Join an existing game" -> {
-                    try {
-                        ViewGUI.queue.put(2);
-
-                    } catch (InterruptedException ex) {
-                        ex.printStackTrace();
-                    }
-                }
-                case "Reconnect to an ongoing game" -> {
-                    try {
-                        ViewGUI.queue.put(3);
-
-                    } catch (InterruptedException ex) {
-                        ex.printStackTrace();
-                    }
-                }
-            }
-        });
+     * Method to handle creation of the game.
+     * Sends the code 1 for the creation of the game to the manager thread
+     */
+    public void handleCreate() {
+        try {
+            ViewGUI.queue.put(1);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
     }
+
+    /**
+     * Method to handle joining of a game.
+     * Sends the code 2 to receive the list of disposable games to the manager thread
+     */
+    public void handleJoin() {
+        try {
+            ViewGUI.queue.put(2);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Method to handle reconnection.
+     * Sends the code 3 to reconnect on a game to the manager thread
+     */
+    public void handleReconnect() {
+        try {
+            ViewGUI.queue.put(3);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
