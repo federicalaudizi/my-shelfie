c65afe54f49aac684054d6862a9eeab05b259bb2
Federico Liuzzi <57435671+fliuzzi02@users.noreply.github.com>
2023-06-27 10:59:55 +0200

    modify konami code

--- src/main/java/it/polimi/ingsw/server/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/server/controller/GameController.java
@@ -270,7 +270,7 @@ public class GameController implements Runnable {
             boolean pickables;
             coordinates = getClientHandler(currentPlayerId).getTiles();
             // Konami Code
-            if(currentPlayerId.equals("Gennaro") && coordinates[0].getColumn() == 9 && coordinates[0].getRow() == 6 && coordinates[1].getColumn() == 2 && coordinates[1].getRow() == 4) {
+            if(currentPlayerId.equals("Gennaro") && coordinates[0].getColumn() == 1 && coordinates[0].getRow() == 5 && coordinates[1].getColumn() == 2 && coordinates[1].getRow() == 4) {
                 getClientHandler(currentPlayerId).sendOk();
                 return true;
             }
