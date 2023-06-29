db970e9504d450409cc7b4938aca527578ef8832
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2023-06-29 16:07:49 +0200

    Remove main in ClientSocket

--- src/main/java/it/polimi/ingsw/client/ClientSocket.java
+++ src/main/java/it/polimi/ingsw/client/ClientSocket.java
@@ -296,15 +296,4 @@ public class ClientSocket extends Client {
         writer.print(message.toString() + "\n");
         writer.flush();
     }
-
-    public static void main(String[] args) {
-        ClientSocket client = new ClientSocket(true);
-
-        try {
-            client.start();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("Exiting...");
-        }
-    }
 }
